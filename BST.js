9beebbb62e7e09ae19b72ebf7774a62ac1b8b06f
ayaalbakri <ayaalbakri89@gmail.com>
2018-01-14 21:23:59 +0200

    binarySearchTree

---
+++ BST.js
@@ -0,0 +1,44 @@
+function BinarySearchTree(value) {
+	// body...
+	this.value = value;
+	this.right = null;
+	this.left = null;
+}
+
+
+BinarySearchTree.prototype.insert = function(value) {
+	// body...
+	if(value<this.value){
+		if (!this.left) {
+			this.left=new BinarySearchTree(value);
+			
+		}
+		if(this.left){
+			this.left.insert(value);
+		    }
+	}
+		if (value>this.value) {
+			if (!this.right) {
+				this.right = new BinarySearchTree(value);
+				
+			}
+			if (this.right) {
+					this.right.insert(value) ;
+				}
+		}
+};
+
+BinarySearchTree.prototype.contain = function(value) {
+	// body...
+	if (this.value === value) {
+		return true;
+	}
+	if (this.value>value) {
+		if(!this.left) return false;
+		else return this.left.contain(value)
+	}
+	if (this.value<value) {
+		if (!this.right) return false;
+		else return this.right.contain(value);
+	}
+};
